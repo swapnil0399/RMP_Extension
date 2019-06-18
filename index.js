f3288c01ac8797f57b2aeb81fa4917185c7eb647
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-06-18 01:55:59 -0500

    Update index with error handling

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 		try {
 			var result = await run(req.query.university, req.query.prof);
 			console.log(result);
-			res.send(result);
+			//res.send(result);
 			// result.then((data) => {
 			// 	console.log(data);
 			// 	res.send(data);
