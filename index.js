2d37a84f8aac649418e1e49ddb0421ab6223dcc1
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-06-18 01:48:11 -0500

    Update index with error handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'); 
 var app = express();
-var exec = require("child_process").exec; 
+var spawn = require("child_process").spawn; 
 
 app.listen(3000, function() { 
 	console.log('server running on port 3000'); 
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
  	(async () => {
 		try {
 			var result = await run(req.query.university, req.query.prof);
-			console.log(result.toString());
-			res.send(result.toString());
+			console.log(result);
+			res.send(result);
 			// result.then((data) => {
 			// 	console.log(data);
 			// 	res.send(data);
@@ -33,44 +33,19 @@ app.get('/', (req, res) => {
 
 function run(univ, prof) {
 	return new Promise ((resolve, reject) => {
-		exec('python',["./scraper.py", univ, prof], (error, stdout, stderr) => {
-			if (error) {
-				console.error(error.trim());
-				return;
-			} else {
-				if(stderr){
-					reject(stderr.trim());
-				} else {
-					resolve(stdout.trim());
-				}
-			}
-		})
+		result = spawn('python',["./scraper.py", univ, prof]);
+		
+		process.stdout.on('data', function(data) {
+			resolve(data.toString());
+		}); 
+
+		process.stderr.on('data', function(error) {
+			reject(error.toString());
+		}); 
+
+		process.on('exit', function(code) {
+			console.log("Exited with code " + code);
+		});
 	})
 }
-// var result, error;
 
-// process.stdout.on('data', function(data) {
-// 	result = data.toString();
-// 	error = false;
-// }); 
-
-// process.stderr.on('data', function(error) {
-// 	result = error.toString();
-// 	error = true;
-// }); 
-
-// process.on('exit', function(code) {
-// 	console.log("Exited with code " + code);
-// });
-
-// if(error){
-// 	return new Promise((resolve, reject) => {
-// 		console.log(result);
-// 		reject(result);
-// 	});
-// } else {
-// 	return new Promise((resolve, reject) => {
-// 		console.log(result);
-// 		resolve(result);
-// 	});
-// }
