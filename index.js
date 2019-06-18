7e0603a801e90f38abf244e37fd277c25c43e249
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-06-17 23:50:14 -0500

    Update index with error handling

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-// import express JS module into app 
-// and creates its variable. 
 var express = require('express'); 
-var app = express(); 
+var app = express();
+var spawn = require("child_process").spawn; 
 
-// Creates a server which runs on port 3000 and 
-// can be accessed through localhost:3000 
 app.listen(3000, function() { 
 	console.log('server running on port 3000'); 
 }) 
 
-// Function callName() is executed whenever 
-// url is of the form localhost:3000/name 
 app.get('/', (req, res) => { 
 	
-	// Use child_process.spawn method from 
-	// child_process module and assign it 
-	// to variable spawn 
-	var spawn = require("child_process").spawn; 
+	console.log(req.query.university);
+	console.log(req.query.prof);
+
+	(() => {
+		try {
+			run();
+		} catch (exp) {
+			console.error(exp.stack);
+			process.exit(1);
+		}
+	})();
 	
-	// Parameters passed in spawn - 
-	// 1. type_of_script 
-	// 2. list containing Path of the script 
-	// and arguments for the script 
+});
+
+function run() {
+	var process = spawn('python',["./test.py", req.query.university, req.query.prof]); 
 	
-	// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-	// so, first name = Mike and last name = Will
-	
-	console.log(req.query.university)
-	console.log(req.query.prof)
-	
-	//var process = spawn('python',["./scraper.py", req.query.university, req.query.prof, timeout=2000000]);
-	
-	//console.log("Request enqueued...")
-	var process = spawn('python',["./test.py", req.query.university, req.query.prof, timeout=20]); 
-	
-	// Takes stdout data from script which executed 
-	// with arguments and send this data to res object 
-	
+	process.stdout.on('data', function(error) {
+		console.log(error.toString());
+		res.send(error.toString());
+	}); 
+
 	process.stdout.on('data', function(data) {
+		console.log(data.toString());
 		res.send(data.toString());
-	}) 
-})
+	}); 
+
+	process.on('exit', function(code) {
+		console.log("Exited with code " + code);
+	});
+}
