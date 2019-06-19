89e29f454caa7214146516617cdb4c00a422fa9d
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-06-19 02:51:22 -0500

    Add sql operations

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ function insertIntoSQL(result){
 		var level = result.Level_of_Diff;
 		var url = result.URL;
 
+		console.log(univ, prof, quality, level, url);
+
 		var query = 'INSERT INTO RECORDS VALUES(' + (++rowCount) + ',"' + univ + '","' + prof + '",' + quality + ',' + level + ',"' + url + '");'
 		conn.query(query, (error, results) => {
 			if(error) throw error;
