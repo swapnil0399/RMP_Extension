617ce64441f9d3b5e6e0d5bda7d8d78e556dc62e
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-06-23 04:38:06 -0500

    Update repo

--- extension_files/popup.js
+++ extension_files/popup.js
@@ -12,16 +12,17 @@ $(document).ready(function() {
         var tabNum = "#" + getTabName(this);
         console.log(tabNum);
         var URL = "http://ec2-54-87-246-226.compute-1.amazonaws.com/"
-        var prof = $("#txt_prof").val().toString();
-        var univ = $("#txt_univ").val().toString();
+        var prof = $(tabNum).find("#txt_prof").val().toString();
+        var univ = $(tabNum).find("#txt_univ").val().toString();
         URL = URL + "?university=" + univ + "&prof=" + prof
         $.get(URL, function(data, status){
             console.log(data);
-
+            data.QUALITY = (data.QUALITY).toFixed(1);
+            data.DIFFICULTY = (data.DIFFICULTY).toFixed(1);
             // Populate data in HTML
             $(tabNum).find('h1#lbl_univ').text(data.UNIVERSITY);
             $(tabNum).find('h2#lbl_prof').text(data.NAME);
-            $(tabNum).find('h1#lbl_qulaity').text(data.QUALITY);
+            $(tabNum).find('h1#lbl_quality').text(data.QUALITY);
             $(tabNum).find('h1#lbl_difficulty').text(data.DIFFICULTY);
             $(tabNum).find('#link_rmp').attr("href", data.URL)
 
@@ -39,6 +40,7 @@ $(document).ready(function() {
             $(tabNum).find('#rating_bar_diff').attr("style", "width: " + ((data.DIFFICULTY/5)*100).toString() + "%");
         });
         $(tabNum).find("#submit").hide();
+        clearTextFields();
     });
 
     $(".reset").click(function(){
@@ -57,18 +59,13 @@ $(document).ready(function() {
 
     function init(){
         $(".loader").hide();
-        $(".tab").each(function(){
-            $(this).hide();
-        });
-        $(".ratings").each(function(){
-            $(this).hide();
-        });
-        $(".link").each(function(){
-            $(this).hide();
-        });
-        $(".reset").each(function(){
-            $(this).hide();
-        });
+        var class_list = [".tab", ".ratings", ".link", ".reset", ".fa-question-circle", ".switch"]
+        
+        for(var i = 0; i < class_list.length; i++){
+            $(class_list[i]).each(function(){
+                $(this).hide();
+            });
+        }
 
         for(var i = 0; i < 4; i++){
             var tab = "#tab_" + (i+1).toString();
@@ -82,6 +79,14 @@ $(document).ready(function() {
         $(".tab").each(function(){
             $(this).hide();
         });
+    }
+
+    function clearTextFields(){
+        for(var i = 0; i < 4; i++){
+            var tab = "#tab_" + (i+1).toString();
+            $(tab).find('#txt_univ').val("");
+            $(tab).find('#txt_prof').val("");
+        }
     }
 
     function getTabName(element){
