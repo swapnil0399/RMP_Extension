cca93636f820bc0ed37b340cef014e16779a6cde
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-08-17 13:13:23 -0500

    Adding cors policy

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.listen(3000, function() {
 
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With");
     next()
 });
 
